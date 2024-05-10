434f2e6f380a08a9c974f30b67ace40217538a5b
lenugo <ljng97@gmail.com>
2024-05-09 20:47:41 -0400

    config: deploy to github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,12 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/logo-gs44.png',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  url: 'https://Lenugo.github.io',
+  baseUrl: '/scout-44/',
+  organizationName: 'Lenugo',
+  projectName: 'scout-44',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
