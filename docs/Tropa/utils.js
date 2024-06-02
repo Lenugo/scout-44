ff68c0bc42d9b3b34f2e1a9cf8141425f57ee5ab
lenugo <ljng97@gmail.com>
2024-06-02 13:03:45 -0400

    patrols score was updated

--- docs/Tropa/utils.js
+++ docs/Tropa/utils.js
@@ -3,11 +3,11 @@
  * to use for update the score week by week
  */
 export const scorePatrols = {
-	lastUpdated: '26/5/2024', // TODO: date time format {d/m/yyyy}
+	lastUpdated: '2/6/2024', // TODO: date time format {d/m/yyyy}
 	patrols : [
-		{ name: 'Patrulla lobo', imgUri: './img/lobo-badge.png', score: 96 },
+		{ name: 'Patrulla lobo', imgUri: './img/lobo-badge.png', score: 176 },
 		{ name: 'Patrulla ardilla', imgUri: './img/ardilla-badge.png', score: 16 },
-		{ name: 'Patrulla cuervo', imgUri: './img/cuervo-badge.png', score: 30 },
+		{ name: 'Patrulla cuervo', imgUri: './img/cuervo-badge.png', score: 110 },
 		{ name: 'Patrulla liebre', imgUri: './img/liebre-badge.png', score: 32 },
 		{ name: 'Patrulla toro', imgUri: './img/toro-badge.png', score: 67 }
 	]
