eec183a627c10ab8f596d8e453320c3e988b123d
lenugo <ljng97@gmail.com>
2024-07-16 11:15:38 -0400

    update score patrols

--- docs/Tropa/utils.js
+++ docs/Tropa/utils.js
@@ -3,12 +3,12 @@
  * to use for update the score week by week
  */
 export const scorePatrols = {
-	lastUpdated: '23/6/2024', // TODO: date time format {d/m/yyyy}
+	lastUpdated: '16/7/2024', // TODO: date time format {d/m/yyyy}
 	patrols : [
-		{ name: 'Patrulla lobo', imgUri: './img/lobo-badge.png', score: 238 },
-		{ name: 'Patrulla ardilla', imgUri: './img/ardilla-badge.png', score: 36 },
-		{ name: 'Patrulla cuervo', imgUri: './img/cuervo-badge.png', score: 135 },
-		{ name: 'Patrulla liebre', imgUri: './img/liebre-badge.png', score: 87 },
-		{ name: 'Patrulla toro', imgUri: './img/toro-badge.png', score: 107 }
+		{ name: 'Patrulla lobo', imgUri: './img/lobo-badge.png', score: 275 },
+		{ name: 'Patrulla ardilla', imgUri: './img/ardilla-badge.png', score: 41 },
+		{ name: 'Patrulla cuervo', imgUri: './img/cuervo-badge.png', score: 151 },
+		{ name: 'Patrulla liebre', imgUri: './img/liebre-badge.png', score: 114 },
+		{ name: 'Patrulla toro', imgUri: './img/toro-badge.png', score: 137 }
 	]
 } 
