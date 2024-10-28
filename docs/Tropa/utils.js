bd4737546eac03f1c22efe5e9def2d398a357cdd
lenugo <ljng97@gmail.com>
2024-10-28 11:06:04 -0400

    score was updated

--- docs/Tropa/utils.js
+++ docs/Tropa/utils.js
@@ -8,7 +8,7 @@ export const scorePatrols = {
     { name: "Patrulla lobo", imgUri: "./img/lobo-badge.png", score: 667 },
     { name: "Patrulla ardilla", imgUri: "./img/ardilla-badge.png", score: 237 },
     { name: "Patrulla cuervo", imgUri: "./img/cuervo-badge.png", score: 304 },
-    { name: "Patrulla liebre", imgUri: "./img/liebre-badge.png", score: 298 },
+    { name: "Patrulla liebre", imgUri: "./img/liebre-badge.png", score: 312 },
     { name: "Patrulla toro", imgUri: "./img/toro-badge.png", score: 464 },
   ],
 };
