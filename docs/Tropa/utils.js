713279757db567b08410120d56996561adc61c1d
lenugo <ljng97@gmail.com>
2024-10-28 09:50:57 -0400

    score was updated

--- docs/Tropa/utils.js
+++ docs/Tropa/utils.js
@@ -3,12 +3,12 @@
  * to use for update the score week by week
  */
 export const scorePatrols = {
-  lastUpdated: "6/10/2024", // TODO: date time format {d/m/yyyy}
+  lastUpdated: "27/10/2024", // TODO: date time format {d/m/yyyy}
   patrols: [
-    { name: "Patrulla lobo", imgUri: "./img/lobo-badge.png", score: 631 },
-    { name: "Patrulla ardilla", imgUri: "./img/ardilla-badge.png", score: 222 },
-    { name: "Patrulla cuervo", imgUri: "./img/cuervo-badge.png", score: 283 },
+    { name: "Patrulla lobo", imgUri: "./img/lobo-badge.png", score: 667 },
+    { name: "Patrulla ardilla", imgUri: "./img/ardilla-badge.png", score: 237 },
+    { name: "Patrulla cuervo", imgUri: "./img/cuervo-badge.png", score: 304 },
     { name: "Patrulla liebre", imgUri: "./img/liebre-badge.png", score: 298 },
-    { name: "Patrulla toro", imgUri: "./img/toro-badge.png", score: 440 },
+    { name: "Patrulla toro", imgUri: "./img/toro-badge.png", score: 464 },
   ],
 };
