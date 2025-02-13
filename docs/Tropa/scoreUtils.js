9bfdb371ed8a6c6a14d16929976f15b5313be303
lenugo <54646168+Lenugo@users.noreply.github.com>
2025-02-12 22:58:48 -0400

    score updated

--- docs/Tropa/scoreUtils.js
+++ docs/Tropa/scoreUtils.js
@@ -3,37 +3,37 @@
  * to use for update the score week by week
  */
 export const scorePatrols = {
-  lastUpdated: "7/2/2025", // TODO: date time format {d/m/yyyy}
+  lastUpdated: "9/2/2025", // TODO: date time format {d/m/yyyy}
   patrols: [
     {
       name: "Patrulla lobo",
       igAccount: "patrullalobo44",
       imgUri: "./img/lobo-badge.png",
-      score: 1081,
+      score: 1157,
     },
     {
       name: "Patrulla ardilla",
       igAccount: "patullaardilla44",
       imgUri: "./img/ardilla-badge.png",
-      score: 864,
+      score: 899,
     },
     {
       name: "Patrulla cuervo",
       igAccount: "patrullacuervo_gs44",
       imgUri: "./img/cuervo-badge.png",
-      score: 492,
+      score: 513,
     },
     {
       name: "Patrulla liebre",
       igAccount: "patrullaliebre44",
       imgUri: "./img/liebre-badge.png",
-      score: 408,
+      score: 433,
     },
     {
       name: "Patrulla toro",
       igAccount: "patrulla_toro44",
       imgUri: "./img/toro-badge.png",
-      score: 1066,
+      score: 1112,
     },
   ],
 };
